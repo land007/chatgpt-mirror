ace7a8cd4ae325ab0c4f61381fded178915f777d
阿七 <yiqiujia@hotmail.com>
2023-05-05 17:12:21 +0800

    test

--- src/chatgpt.service.ts
+++ src/chatgpt.service.ts
@@ -36,10 +36,10 @@ export class ChatGPTService implements OnModuleInit {
     });
   };
 
-  sendMessage(message: string, parentMessageId: string, apiKey: string, model: string): Observable<MessageEvent> {
-    const { systemMessage, maxTokens, defaultModel = 'gpt-3.5-turbo' } = this.configService;
+  sendMessage(message: string, parentMessageId: string, apiKey: string, _model: string): Observable<MessageEvent> {
+    const { systemMessage, maxTokens, model = 'gpt-3.5-turbo' } = this.configService;
     const currentApiKey = apiKey || process.env.OPENAI_API_KEY; // 优先使用传入的 apiKey，如果为空则使用默认的
-    const currentModel = model || defaultModel; // 优先使用传入的 model，如果为空则使用默认的
+    const currentModel = _model || model; // 优先使用传入的 model，如果为空则使用默认的
     let api = this.apis.get(currentApiKey);
     if (!api) {
 	    api = new ChatGPTAPI({
